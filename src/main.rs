f3c0f248737896cda329137e10fabf1252bb29bb
Ted <Dukekamono@hotmail.com>
2019-07-08 02:03:25 -0700

    decalres

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// Some Standard amethyst imports.
+// Might not need all of them. Might need more.
+use amethyst::{
+    assets::Processor,
+    ecs::{ReadExpect, Resources, SystemData},
+    prelude::*,
+    renderer::{
+        pass::DrawFlat2DDesc, types::DefaultBackend, Factory, Format, GraphBuilder, GraphCreator,
+        Kind, RenderGroupDesc, RenderingSystem, SpriteSheet, SubpassBuilder,
+    },
+    utils::application_root_dir,
+    window::{ScreenDimensions, Window, WindowBundle},
+};
+
 fn main() {
     println!("Hello, world!");
 }
