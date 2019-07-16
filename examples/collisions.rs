78271771089cfec6c38b24475462c42bb0077b4c
William Olsen <wolsen@pdx.edu>
2019-07-16 10:52:26 -0700

    added squares and a collison test method

--- examples/collisions.rs
+++ examples/collisions.rs
@@ -11,6 +11,12 @@ trait Entity: event::EventHandler {
     fn display_bounding_box(&self) -> bool {
         false
     }
+
+    fn collision(&self, e: &Entity) -> bool {
+        let us = &self.bounding_box();
+        let them = &e.bounding_box();
+        us.overlaps(them)
+    }
 }
 
 struct Circle {
@@ -25,15 +31,7 @@ impl Circle {
         }
     }
 }
-impl Entity for Circle {
-    /*
-    fn new() -> Self {
-        Circle{
-            location: nalgebra::Point2::<f32>::new(400.0, 300.0),
-            radius: 200.0,
-        }
-    }*/
-    
+impl Entity for Circle {    
     fn bounding_box(&self) -> graphics::Rect {
         graphics::Rect::new(
             self.location[0] - self.radius,
@@ -70,23 +68,74 @@ impl event::EventHandler for Circle {
     }
 }
 
-struct State {
-    entities: Vec<Box<Entity>>,
+
+
+struct Square {
+    location: nalgebra::Point2<f32>,
+    size: f32,
+}
+impl Square {
+    fn new(x: f32, y:f32, s:f32) -> Square {
+        Square{
+            location: nalgebra::Point2::<f32>::new(x, y),
+            size: s,
+        }
+    }
+}
+impl Entity for Square {    
+    fn bounding_box(&self) -> graphics::Rect {
+        graphics::Rect::new(
+            self.location[0] - self.size,
+            self.location[1] - self.size,
+            self.size,
+            self.size,
+        )
+    }
+}
+impl event::EventHandler for Square {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let rectangle = graphics::Mesh::new_rectangle(
+            ctx, 
+            graphics::DrawMode::fill(),
+            self.bounding_box(),
+            graphics::WHITE
+        )?;
+        graphics::draw(ctx, &rectangle, graphics::DrawParam::default())?;
+
+        Ok(())
+    }
 }
 
+struct State {
+    player: Box<Entity>,
+    entities: Vec<Box<Entity>>,
+}
 impl State {
     fn new() -> State {
         let mut v = Vec::<Box<Entity>>::new();
         v.push(Box::new(Circle::new(20.0, 20.0, 20.0)));
         v.push(Box::new(Circle::new(400.0, 300.0, 50.0)));
         v.push(Box::new(Circle::new(100.0, 500.0, 100.0)));
-        State{ entities: v }
+        v.push(Box::new(Square::new(500.0, 50.0, 20.0)));
+        v.push(Box::new(Square::new(700.0, 500.0, 100.0)));
+
+        let p = Box::new(Square::new(400.0, 60.0, 50.0));
+
+        State{ player: p, entities: v }
     }
 }
-
 impl event::EventHandler for State {
     // game loop to update logic... should do something...
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        for i in &mut self.entities {
+            // get a reference to contents of box<Entity>
+            if i.collision(&*self.player) {
+                println!("Collision detected");
+            }
+        }
         Ok(())
     }
 
@@ -95,9 +144,11 @@ impl event::EventHandler for State {
         // clear screen
         graphics::clear(ctx, graphics::BLACK);
 
+        self.player.draw(ctx)?;
+
         for i in &mut self.entities {
             i.draw(ctx)?;
-        }        
+        }
 
         // display to screen
         graphics::present(ctx)?;
