d40b255d00c5d92ec8dcfd1fc2aaef7860762c7e
William Olsen <wolsen@pdx.edu>
2019-07-19 16:14:10 -0700

    Now there is an animated sprite also made use of Rect::fraction instead of own function

--- examples/sprite_sheet.rs
+++ examples/sprite_sheet.rs
@@ -1,5 +1,15 @@
 use ggez::*;
 use graphics::*;
+use std::time::Duration;
+
+
+// helper function to test if smaller rectangle is inside a bigger rectangle
+fn contains(bigger: &Rect, smaller: &Rect) -> bool {
+    bigger.x <= smaller.x 
+    && bigger.y <= smaller.y
+    && bigger.w >= smaller.x + smaller.w
+    && bigger.h >= smaller.y + smaller.h
+}
 
 struct Sprite {
     source_sheet: Image,
@@ -12,22 +22,11 @@ impl Sprite {
     fn new(source: &Image, clip: Rect) -> GameResult<Sprite> {
         let sheet = source.clone();
 
-        fn contains(bigger: &Rect, smaller: &Rect) -> bool {
-            bigger.x <= smaller.x 
-            && bigger.y <= smaller.y
-            && bigger.w >= smaller.x + smaller.w
-            && bigger.h >= smaller.y + smaller.h
-        }
         if !contains(&sheet.dimensions(), &clip) {
             return Err(error::GameError::ResourceLoadError(format!("Clip {:?} not contained in source", clip)));
         }
         let dp = DrawParam::default().src(
-            Rect::new(
-                clip.x / sheet.width() as f32,
-                clip.y / sheet.height() as f32,
-                clip.w / sheet.width() as f32,
-                clip.h / sheet.height() as f32,
-            )
+            Rect::fraction(clip.x, clip.y, clip.w, clip.h, &sheet.dimensions())
         );
 
         Ok(Sprite {
@@ -44,9 +43,62 @@ impl Sprite {
     }
 }
 
+struct AnimateSprite {
+    source_sheet: Image,
+    draw_param: Vec<DrawParam>,
+    current_frame: usize,
+    frame_duration: Duration, // duration of a single frame
+    accumulated_duration: Duration, // how long current frame has been displayed
+}
+impl AnimateSprite {
+    // provide reference to sprite sheet as source
+    // provide vector of clip rectangles as clip
+    //  -- should be pixel values of source image you want drawn
+    fn new(source: &Image, clip: Vec<Rect>) -> GameResult<AnimateSprite> {
+        let sheet = source.clone();
+
+        let mut params = Vec::new();
+        for c in clip {
+            if !contains(&sheet.dimensions(), &c) {
+                return Err(error::GameError::ResourceLoadError(format!("Clip {:?} not contained in source", c)));
+            }
+            params.push(DrawParam::default().src(
+                Rect::fraction(c.x, c.y, c.w, c.h, &sheet.dimensions())
+            ));
+        }
+
+        Ok(AnimateSprite {
+            source_sheet: sheet,
+            draw_param: params,
+            current_frame: 0usize,
+            frame_duration: std::time::Duration::new(0, 250_000_000),
+            accumulated_duration: std::time::Duration::new(0, 0),
+        })
+    }
+    fn update(&mut self, delta: Duration) -> GameResult {
+        self.accumulated_duration += delta;
+        if self.accumulated_duration >= self.frame_duration {
+            self.accumulated_duration -= self.frame_duration;
+            self.current_frame += 1usize;
+        }
+        if self.current_frame >= self.draw_param.len() {
+            self.current_frame = 0usize;
+        }
+        Ok(())
+    }
+    fn draw(&self, ctx: &mut Context, xpos: f32, ypos: f32) -> GameResult {
+        self.source_sheet.draw(
+            ctx,
+            self.draw_param[self.current_frame].dest([xpos, ypos]),
+        )?;
+        Ok(())
+    }
+}
+
 struct State {
     //img: Image,
     sprite: Vec<Sprite>,
+    animated: AnimateSprite,
 }
 impl State {
     fn new(ctx: &mut Context) -> GameResult<State> {
@@ -58,15 +110,26 @@ impl State {
         sprite.push(Sprite::new(&img, Rect::new(128f32, 768f32, 64f32, 64f32))?);
         sprite.push(Sprite::new(&img, Rect::new(320f32, 256f32, 64f32, 64f32))?);
 
+        let mut frames = Vec::new();
+        frames.push(Rect::new(0f32, 320f32, 64f32, 64f32));
+        frames.push(Rect::new(64f32, 320f32, 64f32, 64f32));
+        frames.push(Rect::new(128f32, 320f32, 64f32, 64f32));
+        frames.push(Rect::new(192f32, 320f32, 64f32, 64f32));
+        frames.push(Rect::new(256f32, 320f32, 64f32, 64f32));
+        frames.push(Rect::new(320f32, 320f32, 64f32, 64f32));
+        let animated = AnimateSprite::new(&img, frames)?;
+
         Ok(State {
             //img,
             sprite,
+            animated,
         })
     }
 }
 impl event::EventHandler for State {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        //update
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        let delta = timer::delta(ctx);
+        self.animated.update(delta)?;
         Ok(())
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
@@ -77,6 +140,8 @@ impl event::EventHandler for State {
             s.draw(ctx, dest, dest)?;
             dest += 64f32;
         }
+
+        self.animated.draw(ctx, dest, dest)?;
 
         present(ctx)?;
         timer::yield_now();
