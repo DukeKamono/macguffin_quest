9d0b7c36223baa1877ce46195f6462925ab15078
William Olsen <wolsen@pdx.edu>
2019-07-24 18:31:28 -0700

    new unused functions for level_builder will be used for image tiles

--- src/entities/environment/level_builder.rs
+++ src/entities/environment/level_builder.rs
@@ -1,4 +1,5 @@
-use ggez::Context;
+use ggez::{Context, error::GameError, GameResult};
+use ggez::graphics::{Image, WHITE};
 
 use super::level::Level;
 use super::wall::Wall;
@@ -7,15 +8,57 @@ const WALL_WIDTH: usize = 100;
 const WALL_HEIGHT: usize = 100;
 
 pub struct LevelBuilder {
-    // fields
+    default: Image,
+    floor: Option<Image>,
+    wall: Option<Image>,
+    tile_width: usize,
+    tile_height: usize,
 }
 
 impl LevelBuilder {
-    /*
-    pub fn new() -> LevelBuilder {
-        LevelBuilder { }
+    pub fn new(ctx: &mut Context, default: Option<&Image>) -> LevelBuilder {
+        let default = match default {
+            Some(img) => img.clone(),
+            None => Image::solid(ctx, 64u16, WHITE).unwrap(),
+        };
+        let tile_width = default.width() as usize;
+        let tile_height = default.height() as usize;
+        LevelBuilder {
+            default,
+            floor: None,
+            wall: None,
+            tile_width,
+            tile_height,
+        }
     }
-    */
+
+    fn validate_image_size(&self, image: &Image) -> GameResult {
+        let image_width = image.width() as usize;
+        let image_height = image.height() as usize;
+        if image_width == self.tile_width && image_height == self.tile_height {
+                return Err(GameError::ResourceLoadError(
+                format!(
+                    "Tile dimension mismatch: received ({},{}) expected ({},{})",
+                    image_width, image_height,
+                    self.tile_width, self.tile_height,
+                )
+            ))
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn set_floor_image(&mut self, image: &Image) -> GameResult {
+        self.validate_image_size(image)?;
+        self.floor = Some(image.clone());
+        Ok(())
+    }
+
+    pub fn set_wall_image(&mut self, image: &Image) -> GameResult {
+        self.validate_image_size(image)?;
+        self.wall = Some(image.clone());
+        Ok(())
+    }
 
     //pub from_level_file() -> ???;
 
@@ -39,7 +82,7 @@ impl LevelBuilder {
         }
         // left right
         for x in &[0, (800-WALL_WIDTH)] {
-            for y in (100..(600-WALL_HEIGHT)).step_by(WALL_HEIGHT) {
+            for y in (WALL_HEIGHT..(600-WALL_HEIGHT)).step_by(WALL_HEIGHT) {
                 w.push((*x as f32, y as f32));
                 //println!("{},{}", x, y);
             }
