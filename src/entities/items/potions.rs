a22ea7ec3e3baf55e4e5ae3b6acd3cc3403d15f6
Ted <dukekamono@hotmail.com>
2019-08-13 15:19:26 -0700

    Potion has a texture. Macguffin randomizes textures

--- src/entities/items/potions.rs
+++ src/entities/items/potions.rs
@@ -1,27 +1,38 @@
 use ggez::nalgebra as na;
 use ggez::*;
+use ggez::graphics::{Image, Rect};
+use crate::sprites::*;
+use std::collections::HashMap;
 
-use super::super::{CollideEntity, DrawableEntity};
+use super::super::{CollideEntity, DrawableEntity, Direction, Animations};
 
 pub struct Potions {
     pub x: f32,
     pub y: f32,
-    pub sprite: graphics::Image,
-    pub hitbox: graphics::Rect,
 	pub used: bool,
+	pub sprite: HashMap<(Animations, Direction), AnimatedSprite>,
+	pub animation: (Animations, Direction),
+	pub direction: Direction,
 }
 
 impl Potions {
     pub fn new(ctx: &mut Context, xpos: f32, ypos: f32) -> Potions {
-        let img = graphics::Image::new(ctx, "/blob.png").unwrap();
-        let hb = img.dimensions();
+        let mut sprite = HashMap::new();
+        let sheet = Image::new(ctx, "/items.png").unwrap();
+        let builder = AnimatedBuilder::new(&sheet);
+		
+		sprite.insert(
+            (Animations::Stand, Direction::Down),
+            builder.create_animated(Rect::new(64f32, 128f32, 64f32, 64f32), 1usize).unwrap()
+        );
 
         Potions {
             x: xpos,
             y: ypos,
-            sprite: img,
-            hitbox: hb,
 			used: false,
+			sprite: sprite,
+			animation: (Animations::Stand, Direction::Down),
+			direction: Direction::Down,
 		}
     }
 }
@@ -29,13 +40,15 @@ impl Potions {
 impl DrawableEntity for Potions {
     fn draw(&self, ctx: &mut Context) -> GameResult {
         let dp = graphics::DrawParam::default().dest(na::Point2::new(self.x, self.y));
-        graphics::draw(ctx, &self.sprite, dp)
+        graphics::draw(ctx, self.sprite.get(&self.animation).unwrap(), dp)?;
+
+        Ok(())
     }
 }
 
 impl CollideEntity for Potions {
     fn get_hitbox(&self) -> graphics::Rect {
-        let mut r = self.hitbox;
+        let mut r = self.sprite.get(&self.animation).unwrap().dimensions().unwrap();
         r.x = self.x;
         r.y = self.y;
         r
