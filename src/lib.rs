ea8fa59c489f3b8eb8bfd18847ad9589c3a402a7
William Olsen <wolsen@pdx.edu>
2019-08-13 20:45:41 -0700

    lib comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
-// contains all the information on entities
+/// Contains all things that can interact with each other in game.
+/// Player, Enemies, Items, etc
 pub mod entities;
+
+/// Contains struct useful for building and drawing sprites from a source image.
 pub mod sprites;
+
+/// Contains state manager and various states the game can be in.
 pub mod states;
+
+/// Contains struct useful for displaying UI elements and floating text
 pub mod ui;
